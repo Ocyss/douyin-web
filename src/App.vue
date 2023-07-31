3f72b104e2f8cc0412a99c630a347695c30d9042
Ocyss <qiudie@88.com>
2023-07-31 13:12:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a href="#" target="_blank">
+      <img src="/static/logo.svg" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+  <!-- <h1>极简版抖音</h1> -->
+  <titleVue />
+</template>
+
+<script setup lang="ts">
+import titleVue from './components/title.vue';
+</script>
+
+<style lang="scss">
+body {
+  background-color: #111111;
+}
+</style>
